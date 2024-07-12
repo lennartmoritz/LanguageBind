b97b9632aee38325ae6709edb00b61dfdef87994
Lennart Moritz <1moritz@ltgpu2.informatik.uni-hamburg.de>
2024-07-12 16:42:44 +0200

    add msrvtt eval after language detection

---
+++ msrvtt_dataloader.py
@@ -0,0 +1,87 @@
+import os
+import os.path as osp
+import json
+import pandas as pd
+from easydict import EasyDict
+from torch.utils.data import Dataset
+
+class MSRVTT_Dataset(Dataset):
+    """MSRVTT dataset loader"""
+    def __init__(self, video_folder, audio_folder, asr_folder, lang_detect_json, csv_path):
+        self.video_folder = video_folder
+        self.audio_folder = audio_folder
+        self.asr_folder = asr_folder
+        self.lang_detect_json = lang_detect_json
+
+        # filter data for english asr
+        self.data = pd.read_csv(csv_path)
+        self.data["video_id"] = self.data["video_id"] + ".mp4"
+        lang_detect_set = set(self.get_english_detected_list())
+        self.data = self.data.loc[self.data["video_id"].isin(lang_detect_set)]
+        self.data.reset_index(drop=True, inplace=True)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        video_filename = self.data['video_id'].values[idx].split(".")[0]
+        video_extension = self.data['video_id'].values[idx].split(".")[1]
+
+        sentence = self.data['sentence'].values[idx]
+        video_path = os.path.join(self.video_folder, f"{video_filename}.{video_extension}")
+        audio_path = os.path.join(self.audio_folder, f"{video_filename}.mp3")
+        asr_path = os.path.join(self.asr_folder, f"{video_filename}.txt")
+        with open(asr_path, "r") as asr_file:
+            asr_text = asr_file.read()
+
+        return sentence, video_path, audio_path, asr_text
+    
+    def get_english_detected_list(self):
+        """
+        Return all video names including file extension that contain english ASR.
+        """
+        assert osp.exists(self.lang_detect_json)
+        with open(self.lang_detect_json) as f:
+            language_report = json.load(f)
+        language_report = language_report["details"]
+        # english_list = [k for k, v in language_report.items() if v == "<|en|>"]
+        english_list = []
+        for key, val in language_report.items():
+            if val == "<|en|>":
+                video_filename = key.split(".")[0] + ".mp4"
+                english_list.append(video_filename)
+        return english_list
+
+def get_args_msrvtt():
+    # build args
+    args = {
+        "csv_path": '/raid/1moritz/datasets/MSRVTT/MSRVTT_JSFUSION_test.csv',
+        "language_detect_path": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/report_lang_detect.json',
+        "video_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/videos/all',
+        "audio_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/clip_audios',
+        "asr_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/clip_asr',
+        "batch_size_val": 8,
+        "num_thread_reader": 1,
+        "cache_dir": '/raid/1moritz/models/languagebind/downloaded_weights',
+    }
+    args = EasyDict(args)
+    return args
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    args = get_args_msrvtt()
+    msrvtt_ds = MSRVTT_Dataset(
+        video_folder=args.video_folder,
+        audio_folder=args.audio_folder,
+        asr_folder=args.asr_folder,
+        lang_detect_json=args.language_detect_path,
+        csv_path=args.csv_path,
+    )
+    print("Length: ",len(msrvtt_ds))
+
+    dataloader = DataLoader(msrvtt_ds, batch_size=1, shuffle=True)
+    for i, data in enumerate(dataloader):
+        if i >= 3:
+            break
+        print(f"Sample {i + 1}: {data}")
+
---
+++ msrvtt_eval.py
@@ -0,0 +1,216 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
+from easydict import EasyDict
+from torch.utils.data import DataLoader
+import numpy as np
+import torch
+import os.path as osp
+from tqdm.auto import tqdm as tqdm
+from languagebind import LanguageBind, to_device, transform_dict, LanguageBindImageTokenizer
+from vl_ret.metrics import compute_metrics
+import sys
+from msrvtt_dataloader import MSRVTT_Dataset
+
+
+def get_args_msrvtt():
+    # build args
+    args = {
+        "csv_path": '/raid/1moritz/datasets/MSRVTT/MSRVTT_JSFUSION_test.csv',
+        "language_detect_path": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/report_lang_detect.json',
+        "video_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/videos/all',
+        "audio_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/clip_audios',
+        "asr_folder": '/raid/1moritz/datasets/MSRVTT/original_data/MSRVTT/clip_asr',
+        "batch_size_val": 8,
+        "num_thread_reader": 1,
+        "cache_dir": '/raid/1moritz/models/languagebind/downloaded_weights',
+    }
+    args = EasyDict(args)
+    return args
+
+def run_eval(model:LanguageBind, tokenizer:LanguageBindImageTokenizer, dataloader:DataLoader, modality_transform: dict, device: torch.device):
+    batch_sentences_embeddings, batch_videos_embeddings, batch_audios_embeddings, batch_asr_embeddings = [], [], [], []
+    # Calculate embeddings
+    for bid, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+        sentences, video_paths, audio_paths, asr_texts = batch
+
+        if not isinstance(sentences, list):
+            sentences = list(sentences)
+        if not isinstance(video_paths, list):
+            video_paths = list(video_paths)
+        if not isinstance(audio_paths, list):
+            audio_paths = list(audio_paths)
+        if not isinstance(asr_texts, list):
+            asr_texts = list(asr_texts)
+
+        # print(sentences)
+        # print(type(sentences))
+        # print(video_paths)
+        # print(type(video_paths))
+        # print(audio_paths)
+        # print(type(audio_paths))
+        # sys.exit()
+        inputs = {
+            'video': to_device(modality_transform['video'](video_paths), device),
+            'audio': to_device(modality_transform['audio'](audio_paths), device)
+        }
+        inputs['language'] = to_device(tokenizer(sentences, max_length=77, padding='max_length',
+                                            truncation=True, return_tensors='pt'), device)
+        asr_inputs = {'language': to_device(tokenizer(asr_texts, max_length=77, padding='max_length',
+                                            truncation=True, return_tensors='pt'), device)}
+        
+        with torch.no_grad():
+            embeddings = model(inputs)
+            asr_embeddings = model(asr_inputs)
+
+        batch_sentences_embeddings.append(embeddings['language'])
+        batch_audios_embeddings.append(embeddings['audio'])
+        batch_videos_embeddings.append(embeddings['video'])
+        batch_asr_embeddings.append(asr_embeddings['language'])
+
+    # Create similarity matrix
+    sim_matrix = create_sim_matrix(batch_sentences_embeddings, batch_videos_embeddings)
+
+    # Log metrics Text-to-Video
+    print(f"MSR-VTT sim matrix size: {sim_matrix.shape[0]}, {sim_matrix.shape[1]}")
+    tv_metrics = compute_metrics(sim_matrix)
+    vt_metrics = compute_metrics(sim_matrix.T)
+    print('\t Length-T: {}, Length-V:{}'.format(len(sim_matrix), len(sim_matrix[0])))
+
+    print(f"MSR-VTT Text-to-Video:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+                format(tv_metrics['R1'], tv_metrics['R5'], tv_metrics['R10'], tv_metrics['MR'], tv_metrics['MeanR']))
+    print(f"MSR-VTT Video-to-Text:")
+    print('\t>>>  V2T$R@1: {:.1f} - V2T$R@5: {:.1f} - V2T$R@10: {:.1f} - V2T$Median R: {:.1f} - V2T$Mean R: {:.1f}'.
+                format(vt_metrics['R1'], vt_metrics['R5'], vt_metrics['R10'], vt_metrics['MR'], vt_metrics['MeanR']))
+    
+    # Log metrics Text-to-Audio
+    sim_matrix = create_sim_matrix(batch_sentences_embeddings, batch_audios_embeddings)
+    print(f"MSR-VTT sim matrix size: {sim_matrix.shape[0]}, {sim_matrix.shape[1]}")
+    ta_metrics = compute_metrics(sim_matrix)
+    at_metrics = compute_metrics(sim_matrix.T)
+    print(f"MSR-VTT Text-to-Audio:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+                format(ta_metrics['R1'], ta_metrics['R5'], ta_metrics['R10'], ta_metrics['MR'], ta_metrics['MeanR']))
+    print(f"MSR-VTT Audio-to-Text:")
+    print('\t>>>  A2T$R@1: {:.1f} - A2T$R@5: {:.1f} - A2T$R@10: {:.1f} - A2T$Median R: {:.1f} - A2T$Mean R: {:.1f}'.
+                format(at_metrics['R1'], at_metrics['R5'], at_metrics['R10'], at_metrics['MR'], at_metrics['MeanR']))
+    
+    # Log metrics Audio-to-Video
+    sim_matrix = create_sim_matrix(batch_audios_embeddings, batch_videos_embeddings)
+    print(f"MSR-VTT sim matrix size: {sim_matrix.shape[0]}, {sim_matrix.shape[1]}")
+    av_metrics = compute_metrics(sim_matrix)
+    va_metrics = compute_metrics(sim_matrix.T)
+    print(f"MSR-VTT Audio-to-Video:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+                format(av_metrics['R1'], av_metrics['R5'], av_metrics['R10'], av_metrics['MR'], av_metrics['MeanR']))
+    print(f"MSR-VTT Video-to-Audio:")
+    print('\t>>>  V2A$R@1: {:.1f} - V2A$R@5: {:.1f} - V2A$R@10: {:.1f} - V2A$Median R: {:.1f} - V2A$Mean R: {:.1f}'.
+                format(va_metrics['R1'], va_metrics['R5'], va_metrics['R10'], va_metrics['MR'], va_metrics['MeanR']))
+    
+    # Log metrics Text-to-ASR
+    sim_matrix = create_sim_matrix(batch_sentences_embeddings, batch_asr_embeddings)
+    print(f"MSR-VTT sim matrix size: {sim_matrix.shape[0]}, {sim_matrix.shape[1]}")
+    ta_metrics = compute_metrics(sim_matrix)
+    at_metrics = compute_metrics(sim_matrix.T)
+    print(f"MSR-VTT Text-to-ASR:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+                format(ta_metrics['R1'], ta_metrics['R5'], ta_metrics['R10'], ta_metrics['MR'], ta_metrics['MeanR']))
+    print(f"MSR-VTT ASR-to-Text:")
+    print('\t>>>  Asr2T$R@1: {:.1f} - Asr2T$R@5: {:.1f} - Asr2T$R@10: {:.1f} - Asr2T$Median R: {:.1f} - Asr2T$Mean R: {:.1f}'.
+                format(at_metrics['R1'], at_metrics['R5'], at_metrics['R10'], at_metrics['MR'], at_metrics['MeanR']))
+    
+    # Log metrics ASR-to-Video
+    sim_matrix = create_sim_matrix(batch_asr_embeddings, batch_videos_embeddings)
+    print(f"MSR-VTT sim matrix size: {sim_matrix.shape[0]}, {sim_matrix.shape[1]}")
+    av_metrics = compute_metrics(sim_matrix)
+    va_metrics = compute_metrics(sim_matrix.T)
+    print(f"MSR-VTT ASR-to-Video:")
+    print('\t>>>  R@1: {:.1f} - R@5: {:.1f} - R@10: {:.1f} - Median R: {:.1f} - Mean R: {:.1f}'.
+                format(av_metrics['R1'], av_metrics['R5'], av_metrics['R10'], av_metrics['MR'], av_metrics['MeanR']))
+    print(f"MSR-VTT Video-to-ASR:")
+    print('\t>>>  V2Asr$R@1: {:.1f} - V2Asr$R@5: {:.1f} - V2Asr$R@10: {:.1f} - V2Asr$Median R: {:.1f} - V2Asr$Mean R: {:.1f}'.
+                format(va_metrics['R1'], va_metrics['R5'], va_metrics['R10'], va_metrics['MR'], va_metrics['MeanR']))
+
+def create_sim_matrix(batch_sentences_embeddings, batch_videos_embeddings):
+    """Calculate embedding vector product for similarity and download result to CPU
+    
+        Returns: 
+            sim_matrix (Text X Video)
+    """
+    sim_matrix = []
+    for idx1 in range(len(batch_sentences_embeddings)):
+        sequence_output = batch_sentences_embeddings[idx1]
+        each_row = []
+        for idx2 in range(len(batch_videos_embeddings)):
+            visual_output = batch_videos_embeddings[idx2]
+            b1b2 =  sequence_output @ visual_output.T
+            b1b2 = b1b2.cpu().detach().numpy()
+            each_row.append(b1b2)
+        each_row = np.concatenate(tuple(each_row), axis=-1)
+        sim_matrix.append(each_row)
+    sim_matrix = np.concatenate(tuple(sim_matrix), axis=0)
+    return sim_matrix
+
+def main():
+    device = 'cuda:0'
+    device = torch.device(device)
+    clip_type = {
+        'video': 'LanguageBind_Video_FT',  # also LanguageBind_Video
+        'audio': 'LanguageBind_Audio_FT',  # also LanguageBind_Audio
+        'image': 'LanguageBind_Image',
+    }
+    args = get_args_msrvtt()
+
+    model = LanguageBind(clip_type=clip_type, cache_dir=args.cache_dir)
+    model = model.to(device)
+    model.eval()
+    pretrained_ckpt = f'LanguageBind/LanguageBind_Image'
+    tokenizer = LanguageBindImageTokenizer.from_pretrained(pretrained_ckpt, cache_dir=osp.join(args.cache_dir, 'tokenizer_cache_dir'))
+    modality_transform = {c: transform_dict[c](model.modality_config[c]) for c in clip_type.keys()}
+
+    dataloader_msrvtt = DataLoader(
+        MSRVTT_Dataset(
+            video_folder=args.video_folder, 
+            audio_folder=args.audio_folder,
+            asr_folder=args.asr_folder,
+            lang_detect_json=args.language_detect_path,
+            csv_path=args.csv_path),
+        batch_size=args.batch_size_val,
+        num_workers=args.num_thread_reader,
+        shuffle=False,
+        drop_last=False,
+    )
+    run_eval(model, tokenizer, dataloader_msrvtt, modality_transform, device)
+
+
+if __name__ == '__main__':
+    main()
+"""
+MSR-VTT sim matrix size: 619, 619
+	 Length-T: 619, Length-V:619
+MSR-VTT Text-to-Video:
+	>>>  R@1: 48.1 - R@5: 73.7 - R@10: 81.1 - Median R: 2.0 - Mean R: 13.5
+MSR-VTT Video-to-Text:
+	>>>  V2T$R@1: 46.4 - V2T$R@5: 71.8 - V2T$R@10: 80.9 - V2T$Median R: 2.0 - V2T$Mean R: 14.6
+MSR-VTT sim matrix size: 619, 619
+MSR-VTT Text-to-Audio:
+	>>>  R@1: 2.4 - R@5: 8.6 - R@10: 12.9 - Median R: 102.0 - Mean R: 158.1
+MSR-VTT Audio-to-Text:
+	>>>  A2T$R@1: 1.3 - A2T$R@5: 5.1 - A2T$R@10: 8.8 - A2T$Median R: 137.0 - A2T$Mean R: 182.5
+MSR-VTT sim matrix size: 619, 619
+MSR-VTT Audio-to-Video:
+	>>>  R@1: 2.9 - R@5: 11.0 - R@10: 15.5 - Median R: 119.0 - Mean R: 175.7
+MSR-VTT Video-to-Audio:
+	>>>  V2A$R@1: 5.3 - V2A$R@5: 11.8 - V2A$R@10: 17.9 - V2A$Median R: 86.0 - V2A$Mean R: 137.2
+MSR-VTT sim matrix size: 619, 619
+MSR-VTT Text-to-ASR:
+	>>>  R@1: 17.9 - R@5: 28.5 - R@10: 33.3 - Median R: 45.0 - Mean R: 130.0
+MSR-VTT ASR-to-Text:
+	>>>  Asr2T$R@1: 15.0 - Asr2T$R@5: 23.8 - Asr2T$R@10: 27.4 - Asr2T$Median R: 108.0 - Asr2T$Mean R: 179.7
+MSR-VTT sim matrix size: 619, 619
+MSR-VTT ASR-to-Video:
+	>>>  R@1: 33.8 - R@5: 47.7 - R@10: 54.8 - Median R: 7.0 - Mean R: 68.9
+MSR-VTT Video-to-ASR:
+	>>>  V2Asr$R@1: 31.0 - V2Asr$R@5: 47.3 - V2Asr$R@10: 53.7 - V2Asr$Median R: 7.5 - V2Asr$Mean R: 57.8
+"""
